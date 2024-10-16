c3a814516254bb4a2537bf5ff6e0255cac737134
jpucavalcante <jpucavalcante@gmail.com>
2024-10-16 18:31:50 -0300

    refactor: sitemap_reader handling of date value

--- scripts/sitemap_reader.py
+++ scripts/sitemap_reader.py
@@ -1,6 +1,14 @@
 import requests
 from lxml import etree
 
+def sitemap_item_handler(root, item, item_val, sitemap_items, n_of_items):
+    if item == "date_info":
+        sitemap_items['date'] = [child.text for child in root.xpath(item_val['date'])[:n_of_items]]
+        sitemap_items['date_format'] = [item_val['date_format']]*n_of_items
+    else:
+        sitemap_items[item] = [child.text for child in root.xpath(item_val)[:n_of_items]]
+        
+
 def get_sitemap_items(url: str, sitemap_paths: dict, n_of_items: int = 10) -> dict:
     try:
         response =  requests.get(url)
@@ -12,11 +20,8 @@ def get_sitemap_items(url: str, sitemap_paths: dict, n_of_items: int = 10) -> di
         root = etree.fromstring(response.content)
 
         sitemap_items = {}
-        sitemap_items['date'] = [child.text for child in root.xpath(sitemap_paths['date_info']['date'])[:n_of_items]]
-        sitemap_items['date_format'] = [sitemap_paths['date_info']['date_format']]*n_of_items
-        for item, xpath in sitemap_paths.items():
-            if item != "date_info":
-                sitemap_items[item] = [child.text for child in root.xpath(xpath)[:n_of_items]]
+        for item, item_val in sitemap_paths.items():
+            sitemap_item_handler(root, item, item_val, sitemap_items, n_of_items)
         return sitemap_items
     else:
         print('Request error {response.status_code}')
